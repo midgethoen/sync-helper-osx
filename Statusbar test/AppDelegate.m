f941ed91b5da357a4f9de0eca4d8b0bf3a514346
Midge 't Hoen <midgethoen@MacBook-Pro-van-Midge.local>
2013-05-03 14:12:07 +0200

    Initial Commit

---
+++ Statusbar test/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Statusbar test
+//
+//  Created by Midge 't Hoen on 03-05-13.
+//  Copyright (c) 2013 Midge 't Hoen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Statusbar test/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Statusbar test
+//
+//  Created by Midge 't Hoen on 03-05-13.
+//  Copyright (c) 2013 Midge 't Hoen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
